fb7d583b26431abee549c307027d5560eee93334
Alyson Pereira <alyson.pereira@beeleads.com.br>
2017-07-27 16:40:16 -0300

    Até sdep implementado

---
+++ src/Control/Ciclo3/ControlS_Dep.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control.Ciclo3;
+
+/**
+ *
+ * @author alysonmp
+ */
+public class ControlS_Dep {
+    
+    double Pc, Tc, Tr, Pr, F, Omegab, Omegaa, Omegac, A, B, C, N, M, Q, alfa, a, da_dT, Ps2, dif, SDL, SDV;
+    
+    public ControlS_Dep(double T, double P){
+        
+        ControlConstantes constantes = new ControlConstantes(T, P);
+        
+        ControlZeta zeta = new ControlZeta(constantes.getAi(), constantes.getBi(), constantes.getCi());
+        
+        Pc = constantes.getPci();
+        Tc = constantes.getTci();
+        Tr = constantes.getTri();
+        Pr = constantes.getPri();
+        F = constantes.getFi();
+        Omegab = constantes.getOmegabi();
+        Omegaa = constantes.getOmegaai();
+        Omegac = constantes.getOmegaci();
+        A = constantes.getAi();
+        B = constantes.getBi();
+        C = constantes.getCi();
+        N = constantes.getNi();
+        M = constantes.getMi();
+        Q = constantes.getQi();
+        alfa = constantes.getalfai();
+        a = constantes.getai();
+        
+        da_dT = Omegaa*(Math.pow(constantes.getR(),2)*Tc/Pc)*(-F*(Math.pow(alfa,0.5))/(Math.pow(Tr, 0.5)));
+        
+        ControlPdeVapor pdevapor = new ControlPdeVapor(T);
+        Ps2 = pdevapor.getPs2();
+        
+        if(Ps2 <= 73.825){
+            dif = Math.abs((Ps2-P)/P);
+            if(dif >= 0.001 && Ps2<P){
+                //%disp('Esta liquido') ;
+                SDL=((-constantes.getR()*Math.log(P/(zeta.getZl()-B)))-(da_dT*((1/(2*N))*(Math.log((zeta.getZl()+M)/(zeta.getZl()+Q))))))*100000;
+                SDV=0;
+            }else{
+                if(dif >= 0.001 && Ps2>P){
+                    //% disp('Esta como vapor') ;
+                    SDL=0;
+                    SDV=((-constantes.getR()*Math.log(P/(zeta.getZv()-B)))-(da_dT*((1/(2*N))*(Math.log((zeta.getZv()+M)/(zeta.getZv()+Q))))))*100000;
+                }else{
+                    //%disp('Esta no Ponto de saturaÁ„o') ;
+                    SDL=((-constantes.getR()*Math.log(P/(zeta.getZl()-B)))-(da_dT*((1/(2*N))*(Math.log((zeta.getZl()+M)/(zeta.getZl()+Q))))))*100000;
+                    SDV=((-constantes.getR()*Math.log(P/(zeta.getZv()-B)))-(da_dT*((1/(2*N))*(Math.log((zeta.getZv()+M)/(zeta.getZv()+Q))))))*100000;
+                }
+            }
+        }else{
+            SDL=((-constantes.getR()*Math.log(P/(zeta.getZl()-B)))-(da_dT*((1/(2*N))*(Math.log((zeta.getZl()+M)/(zeta.getZl()+Q))))))*100000;
+            SDV=((-constantes.getR()*Math.log(P/(zeta.getZv()-B)))-(da_dT*((1/(2*N))*(Math.log((zeta.getZv()+M)/(zeta.getZv()+Q))))))*100000;
+        }
+    } 
+
+    public double getSDL() {
+        return SDL;
+    }
+
+    public void setSDL(double SDL) {
+        this.SDL = SDL;
+    }
+
+    public double getSDV() {
+        return SDV;
+    }
+
+    public void setSDV(double SDV) {
+        this.SDV = SDV;
+    }
+}
---
+++ src/Control/Ciclo3/ControlT_Ref.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control.Ciclo3;
+
+/**
+ *
+ * @author alysonmp
+ */
+public class ControlT_Ref {
+    
+    double Te, erro, DT, it, Ps2, Burbuja, Tref;
+    
+    public ControlT_Ref(double P){
+        Te = 300;
+        erro = 1;
+        DT = 80;
+        it = 0;
+        while(erro>0.001){
+            it++;
+            if(it>10000){
+                //ksakfdsak*ksadksda;
+            }
+            
+            ControlPdeVapor pdevapor = new ControlPdeVapor(Te);    
+            Ps2 = pdevapor.getPs2();
+            Burbuja = Ps2-P;
+            erro = Math.abs((Ps2-P)/Ps2);
+            if(erro>0.001 && Burbuja>0){
+                Te = Te-DT;
+                DT = DT/2;
+                if(DT<0.005){
+                   DT = 0.0047953543;
+                }
+            }else{
+                if(erro>0.001 && Burbuja<0){
+                    Te = Te+DT;
+                    DT = DT/2;
+                    if(DT<0.005){
+                        DT=0.00393845211111;
+                    }
+                }
+            }
+        }
+        Tref = Te;
+    }   
+}
+
