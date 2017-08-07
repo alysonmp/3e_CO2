6de56ab2a97501c29ac7e31137f77e87d82b6747
leoocastroo <leoocasto7@gmail.com>
2017-08-07 15:32:57 -0300

    Models

---
+++ src/Ciclo3/Model/ModelConsEXE_B.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ciclo3.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ConsEXE_B")
+public class ModelConsEXE_B {
+
+    @Id
+    @GeneratedValue
+    private int cod;
+    
+    @Column
+    private double[] B;
+
+    public ModelConsEXE_B() {
+    }
+    
+    public ModelConsEXE_B(double[] B) {
+        this.B = B;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public double[] getB() {
+        return B;
+    }
+
+    public void setB(double[] B) {
+        this.B = B;
+    }
+}
